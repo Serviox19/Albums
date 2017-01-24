1aa56fa5bdf3087e5fc07d87d3e0bf81ac278e65
Servio Mora <servio.mora19@gmail.com>
2017-01-23 19:30:08 -0500

    learned to style components in react native

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { Text } from 'react-native';
 
 const Header = function () {
+
+  const { textStyle } = styles;
+
   return (
-    <Text>Albums!</Text>
+    <Text style={textStyle}>Albums!</Text>
   );
 };
 
-// const Header = () => {
-//   return (
-//     <Text>Albums</Text>
-//   );
-// };
+const styles = {
+  textStyle: {
+    fontSize: 25,
+    fontWeight: '400',
+    marginTop: 20,
+    marginBottom: 20,
+    color: 'red',
+    textAlign: 'center'
+  }
+};
 
 
 export default Header;
