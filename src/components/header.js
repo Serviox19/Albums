8573d47c078dd8eafba42fda0f2aa1828f5fbb42
Servio Mora <servio.mora19@gmail.com>
2017-01-23 19:55:01 -0500

    mastering styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = function () {
 
-  const { textStyle } = styles;
+  const { viewStyle, textStyle } = styles;
 
   return (
-    <Text style={textStyle}>Albums!</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
   );
 };
 
 const styles = {
+  viewStyle: {
+    height: 60,
+    backgroundColor: '#F8F8F8',
+    marginTop: 22,
+    marginBottom: 20
+  },
   textStyle: {
+    height: 30,
     fontSize: 25,
-    fontWeight: '400',
-    marginTop: 20,
-    marginBottom: 20,
-    color: 'red',
-    textAlign: 'center'
+    fontWeight: '500',
+    color: 'blue',
+    textAlign: 'center',
+    alignItems: 'center',
+    marginVertical: 15
   }
 };
 
