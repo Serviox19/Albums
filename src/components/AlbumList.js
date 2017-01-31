9560f9293bea79dff7d4e3e7f115e873e02b0b13
Servio Mora <servio.mora19@gmail.com>
2017-01-30 20:19:23 -0500

    successfully pulling data from api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 //class based component!
 class AlbumList extends Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(function (response) {
+      const albums = response.data;
+      for (var i = 0; i < albums.length; i++) {
+        console.log(albums[i]);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <View>
